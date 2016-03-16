e8a7307b12993fe1a76e6535065e9e08ba490cfe
ccastilaw <cody.castilaw@gmail.com>
2016-03-16 15:00:44 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,8 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-  res.end('TejasRE.net test is working!\n');
-  res.end('That's right, Cody is awesome\n');
+  res.end('Hello World\nTejasre test site is working\nCody is awesome');
 }).listen(8080, 'localhost');
 console.log('Server running at http://localhost:8080/');
